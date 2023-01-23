387522c1c07c011717506672052cd5683c8617eb
arumandesu <zh.arumandes@gmail.com>
2023-01-23 15:09:45 +0600

    1.Folder structure 2.Router 3.DB connection 4.Dotenv

---
+++ README.md
@@ -0,0 +1,27 @@
+# Not Kino
+
+### Description :
+Backend server of website NK
+
+---
+
+### Resources and tools used:
+- [Visual Studio Code (code editor)](https://code.visualstudio.com)
+- [WebStorm (IDE)](https://www.jetbrains.com/ru-ru/webstorm/)
+- [Gitkraken (Git GUI)](https://www.gitkraken.com/)
+- [MongoDB (Database)](https://www.mongodb.com)
+- [Node.js (official site)](https://nodejs.org/en/)
+- [Node Package Manager](https://www.npmjs.com)
+- [Express (Node.js framework)](https://expressjs.com/ru/)
+
+
+
+---
+### How to run
+```shell
+npm run dev
+or
+npm run start
+or
+nodemon server.js
+```
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.get('/',(req, res)=>{
-    res.send('Hello World!')
-})
+const app = express();
+const router = require("./routes/router");
+const port = process.env.PORT || 4000;
 
-app.listen(4000)
+dotenv.config();
+mongoose.set("strictQuery", false);
+app.use(router);
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then((res) => console.log("Connected to DB"))
+  .catch((error) => console.log(error));
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
