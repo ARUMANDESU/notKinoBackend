e82347fc99bf39b8ce584fd533f803dde2db47ac
arumandesu <zh.arumandes@gmail.com>
2023-02-08 10:35:50 +0600

    mailer

--- utils/mailer/nodemailer.js
+++ utils/mailer/nodemailer.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: process.env.MAILER_HOST,
-    port: 587,
+    port: process.env.MAILER_PORT,
     auth: {
         user: process.env.MAILER_USERNAME,
         pass: process.env.MAILER_PASSWORD,
